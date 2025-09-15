dfbdf48366d5ae3d6f0dc5290839f5da4fa2885c
Tejash Tarun <tejashtarun@Tejashs-MacBook-Air-6.local>
2025-09-15 20:32:52 +0530

    fix: update CSP policy in next.config.ts for Render

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
-const devCsp = "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src * blob: data:; connect-src *; font-src 'self';";
-const prodCsp = "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src * blob: data:; connect-src *; font-src 'self';";
+const devCsp = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  connect-src *;
+  font-src 'self';
+`;
+
+const prodCsp = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  connect-src *;
+  font-src 'self';
+`;
 
 const nextConfig: NextConfig = {
   async headers() {
